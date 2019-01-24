2fd2348e7f002158acc3106498e95c8f66b87a9c
Emilen <emilianknysak@gmail.com>
2019-01-24 11:37:09 +0100

    Zadanie-6a

--- src/CalcTest.java
+++ src/CalcTest.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
+
 public class CalcTest {
     public static void main(String[] args) {
-        Calc calc = new Calc();
-        if(calc.number1 > calc.number2) {
-            System.out.println(calc.sum1);
-        } else {
-            System.out.println(calc.sum2);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę");
+        int firstNumber = input.nextInt();
+        System.out.println("Podaj drugą liczbę");
+        int secondNumber = input.nextInt();
+        if (firstNumber > secondNumber) {
+            System.out.println("Pierwsza liczba większa od drugiej:");
+        } else if (firstNumber < secondNumber) {
+            System.out.println("PIerwsza liczba mniejsza od drugiej:");
+        } else if (firstNumber == secondNumber * 2) {
+            System.out.println("Liczby są równe i podniesione do kwadratu:");
         }
-        if(calc.number1 == calc.number2) {
-            System.out.println("Wspólna potęga: " + calc.sum3 + "\n" + "Potęga liczby pierwszej: " + calc.sum4
-            + "\n" + "Potęga liczby drugiej: " + calc.sum5);
-        }
+
     }
 }
